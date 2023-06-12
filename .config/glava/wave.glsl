30cf06eb438de11f2970b8bf3fec284f53ffc2bd
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2023-06-12 22:18:02 +0530

    Add files via upload

---
+++ .config/glava/wave.glsl
@@ -0,0 +1,11 @@
+/* min (vertical) line thickness */
+#define MIN_THICKNESS 1
+
+/* max (vertical) line thickness */
+#define MAX_THICKNESS 6
+
+#define COLOR vec4(0.04, 0.09, 0.11, 1.0)
+
+/* amplitude */
+#define AMPLIFY 500
+

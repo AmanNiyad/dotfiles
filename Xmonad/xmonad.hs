6a5221752b08909f21813fc4010d0e5989a4b5b3
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2023-07-02 15:01:14 +0530

    Update xmonad.hs

--- Xmonad/xmonad.hs
+++ Xmonad/xmonad.hs
@@ -189,12 +189,6 @@ myManageHook = composeAll
 ------------------------------------------------------------------------
 -- Event handling
 
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
 myEventHook = mempty
 
 ------------------------------------------------------------------------

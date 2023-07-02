97c39a1de2e1ee2f4d911916bb2c3b1eb5ec903b
AmanNiyad <101011357+AmanNiyad@users.noreply.github.com>
2023-07-02 15:01:34 +0530

    Update xmonad.hs

--- Xmonad/xmonad.hs
+++ Xmonad/xmonad.hs
@@ -168,18 +168,6 @@ myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
 ------------------------------------------------------------------------
 -- Window rules:
 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
 myManageHook = composeAll
     [ className =? "Darktable"        --> doFloat
     , className =? "Gimp"           --> doFloat
